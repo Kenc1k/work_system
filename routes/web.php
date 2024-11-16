d2b58677035d276ab09dc6cb61a69639da043635
Kenc1k <iskandarisabrkov@gmail.com>
2024-11-16 13:55:33 +0500

    "users crud done "

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/' , [UserController::class , "index"]);
+Route::get('/user_create' , [UserController::class , 'create'])->name('users.create');
+Route::delete('/users/{user}' , [UserController::class , 'destroy'])->name('users.destroy');
+Route::get('/user/{user}/edit' , [UserController::class , 'edit'])->name('users.edit');
+Route::post('/user_store' , [UserController::class , 'store'])->name('users.store');
+Route::put('/user/{user}' , [UserController::class , 'update'])->name('users.update');
